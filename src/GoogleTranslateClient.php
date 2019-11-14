ca153a33fac486cd9fffa4b09f5e9d6e3e6062fe
Ross Steele <Ross@digitalbalance.co.uk>
2019-11-14 10:45:14 +0000

    Allow format option to be overridden

--- src/GoogleTranslateClient.php
+++ src/GoogleTranslateClient.php
@@ -33,16 +33,16 @@ class GoogleTranslateClient
             ->detectLanguageBatch($input);
     }
 
-    public function translate(string $text, string $translateTo)
+    public function translate(string $text, string $translateTo, string $format = 'text')
     {
         return $this->translate
-            ->translate($text, ['target' => $translateTo, 'format' => 'text']);
+            ->translate($text, ['target' => $translateTo, 'format' => $format]);
     }
 
-    public function translateBatch(array $input, string $translateTo)
+    public function translateBatch(array $input, string $translateTo, string $format = 'text')
     {
         return $this->translate
-            ->translateBatch($input, ['target' => $translateTo, 'format' => 'text']);
+            ->translateBatch($input, ['target' => $translateTo, 'format' => $format]);
     }
 
     public function getAvaliableTranslationsFor(string $languageCode)
