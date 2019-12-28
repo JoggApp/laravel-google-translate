7d2422ab6f8399de843e0cc921774df4a271ed4c
Flears <49255915+flearsnetwork@users.noreply.github.com>
2019-12-28 23:16:04 +0100

    Formatting

--- config/googletranslate.php
+++ config/googletranslate.php
@@ -1,21 +1,21 @@
 <?php
 
 return [
-  /*
-    |----------------------------------------------------------------------------------------------------
-    | The ISO 639-1 code of the language in lowercase to which the text will be translated to by default.
-    |----------------------------------------------------------------------------------------------------
-    */
-  'default_target_translation' => 'en',
+    /*
+      |----------------------------------------------------------------------------------------------------
+      | The ISO 639-1 code of the language in lowercase to which the text will be translated to by default.
+      |----------------------------------------------------------------------------------------------------
+      */
+    'default_target_translation' => 'en',
 
-  /*
-    |-------------------------------------------------------------------------------
-    | Api Key generated within Google Cloud Dashboard.
-    |
-    | The process to get this file is documented in a step by step detailed manner
-    | over here:
-    | https://github.com/JoggApp/laravel-google-translate/blob/master/google.md
-    |-------------------------------------------------------------------------------
-    */
-  'api_key' => env('GOOGLE_TRANSLATE_API_KEY'),
+    /*
+      |-------------------------------------------------------------------------------
+      | Api Key generated within Google Cloud Dashboard.
+      |
+      | The process to get this file is documented in a step by step detailed manner
+      | over here:
+      | https://github.com/JoggApp/laravel-google-translate/blob/master/google.md
+      |-------------------------------------------------------------------------------
+      */
+    'api_key' => env('GOOGLE_TRANSLATE_API_KEY'),
 ];
--- src/GoogleTranslateClient.php
+++ src/GoogleTranslateClient.php
@@ -8,66 +8,65 @@ use JoggApp\GoogleTranslate\Traits\SupportedLanguages;
 
 class GoogleTranslateClient
 {
-  use SupportedLanguages;
+    use SupportedLanguages;
 
-  private $translate;
+    private $translate;
 
-  public function __construct(array $config)
-  {
-    $this->checkForInvalidConfiguration($config);
-
-    $this->translate = new TranslateClient([
-      'key' => $config['api_key']
-    ]);
-  }
-
-  public function detectLanguage(string $text)
-  {
-    return $this->translate
-      ->detectLanguage($text);
-  }
-
-  public function detectLanguageBatch(array $input)
-  {
-    return $this->translate
-      ->detectLanguageBatch($input);
-  }
-
-  public function translate(string $text, string $translateTo, string $format = 'text')
-  {
-    return $this->translate
-      ->translate($text, ['target' => $translateTo, 'format' => $format]);
-  }
-
-  public function translateBatch(array $input, string $translateTo, string $format = 'text')
-  {
-    return $this->translate
-      ->translateBatch($input, ['target' => $translateTo, 'format' => $format]);
-  }
-
-  public function getAvaliableTranslationsFor(string $languageCode)
-  {
-    return $this->translate
-      ->localizedLanguages(['target' => $languageCode]);
-  }
-
-  private function checkForInvalidConfiguration(array $config)
-  {
-    if (!isset($config['api_key']) || $config['api_key'] === null) {
-      throw new Exception('Google Api Key is required.');
+    public function __construct(array $config)
+    {
+        $this->checkForInvalidConfiguration($config);
+        $this->translate = new TranslateClient([
+            'key' => $config['api_key'],
+        ]);
     }
 
-    $codeInConfig = $config['default_target_translation'];
+    public function detectLanguage(string $text)
+    {
+        return $this->translate
+            ->detectLanguage($text);
+    }
 
-    $languageCodeIsValid = is_string($codeInConfig)
-      && ctype_lower($codeInConfig)
-      && in_array($codeInConfig, $this->languages());
+    public function detectLanguageBatch(array $input)
+    {
+        return $this->translate
+            ->detectLanguageBatch($input);
+    }
 
-    if (!$languageCodeIsValid) {
-      throw new Exception(
-        'The default_target_translation value in the config/googletranslate.php file should
+    public function translate(string $text, string $translateTo, string $format = 'text')
+    {
+        return $this->translate
+            ->translate($text, ['target' => $translateTo, 'format' => $format]);
+    }
+
+    public function translateBatch(array $input, string $translateTo, string $format = 'text')
+    {
+        return $this->translate
+            ->translateBatch($input, ['target' => $translateTo, 'format' => $format]);
+    }
+
+    public function getAvaliableTranslationsFor(string $languageCode)
+    {
+        return $this->translate
+            ->localizedLanguages(['target' => $languageCode]);
+    }
+
+    private function checkForInvalidConfiguration(array $config)
+    {
+        if ( ! isset($config['api_key']) || $config['api_key'] === null) {
+            throw new Exception('Google Api Key is required.');
+        }
+
+        $codeInConfig = $config['default_target_translation'];
+
+        $languageCodeIsValid = is_string($codeInConfig)
+            && ctype_lower($codeInConfig)
+            && in_array($codeInConfig, $this->languages());
+
+        if ( ! $languageCodeIsValid) {
+            throw new Exception(
+                'The default_target_translation value in the config/googletranslate.php file should
                 be a valid lowercase ISO 639-1 code of the language'
-      );
+            );
+        }
     }
-  }
 }
